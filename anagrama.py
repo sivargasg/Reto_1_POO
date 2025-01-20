3b6cab12f8a2c322d76f14597d05b32d90643e7e
sivargasg <sivargasg@unal.edu.co>
2025-01-20 15:05:00 -0500

    Update anagrama.py

--- anagrama.py
+++ anagrama.py
@@ -1,4 +1,4 @@
-def compruebe(entrada):
+def compruebe(entrada) -> bool:
     """ 
     Comprueba si la entrada contiene más de una palabra. 
     
@@ -29,13 +29,13 @@ def anagramas(entrada):
     diccionario_anagramas = {}
     for palabra in lista:
         palabra_ordenada = ''.join(sorted(palabra))
-        if palabra_ordenada in diccionario_anagramas: #palabra_ordenada se vuelve una clave dentro del diccionario
+        if palabra_ordenada in diccionario_anagramas: #se busca si palabra_ordenada ya es una clave dentro del diccionario
             diccionario_anagramas[palabra_ordenada].append(palabra) #Se añade la palabra como otro valor de una clave ya usada
         else:
-            diccionario_anagramas[palabra_ordenada] = [palabra] #Se pone la palabra dada como el único valor para la clave
+            diccionario_anagramas[palabra_ordenada] = [palabra] #Se pone la palabra_ordenada como clave y se le asigna el primer valor
     for grupo in diccionario_anagramas.values():
         if len(grupo) > 1:
-            lista_anagramas.extend(grupo)
+            lista_anagramas.extend(grupo) #Revisa los grupos de cada palabra_ordenada y pasa los que tienen más de un valor
     return lista_anagramas
 
 if __name__ == "__main__":
